e338030e0719020d5fd6e7ef33a33da8e7c6f7cd
parulsheel01 <sheelparul7017@gmail.com>
2026-09-09 00:13:48 +0530

    initial backend setup

---
+++ enerion-backend/src/main/java/com/enerionenergy/enerion_backend/EnerionBackendApplication.java
@@ -0,0 +1,13 @@
+package com.enerionenergy.enerion_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnerionBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnerionBackendApplication.class, args);
+	}
+
+}
---
+++ enerion-backend/src/test/java/com/enerionenergy/enerion_backend/EnerionBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enerionenergy.enerion_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnerionBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
